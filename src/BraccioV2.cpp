2795b1363cb4eb4f9d0686ffd941094d3db7311e
kk6axq <kk6axq@users.noreply.github.com>
2019-05-26 13:15:31 -0700

    Initial Commit

--- src/BraccioV2.cpp
+++ src/BraccioV2.cpp
@@ -1,10 +1,26 @@
+/*
+ BraccioV2.cpp - version 0.1
+ Written by Lukas Severinghaus
+ Based upon the Braccio library by Andrea Martino and Angelo Ferrante
+ originally licensed under GNU GPL V1.2.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+
 #include "BraccioV2.h"
-
-
 Braccio::Braccio() {
-
-  //_currentJointPositions[7];
-  //_targetJointPositions[7];
 }
 
 void Braccio::begin() {
--- src/BraccioV2.h
+++ src/BraccioV2.h
@@ -1,9 +1,21 @@
-//Init - default pos, or none
-//Set absolute
-//Set relative
-//Update
-//_softStart();
-//_pwm
+/*
+ BraccioV2.h - version 0.1
+ Written by Lukas Severinghaus
+ Based upon the Braccio library by Andrea Martino and Angelo Ferrante
+ originally licensed under GNU GPL V1.2.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #ifndef BRACCIOV2_H_
 #define BRACCIOV2_H_
 
@@ -32,7 +44,7 @@
 //Max/min values for all servos globally
 #define GLOBAL_MIN 0
 #define GLOBAL_MAX 180
-//Arg input: 
+//Arg input:
 class Braccio {
   public:
     Braccio();
